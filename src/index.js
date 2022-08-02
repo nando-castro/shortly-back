dcf279c1f33a528de5aa5b2c7498da145d8b1b44
nando-castro <fernando.castro10.master@gmail.com>
2022-08-02 09:38:08 -0300

    connection database

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is litening on port ${PORT}`);
+});
