3e238478e3c34c33fa6061c033b636dbed45ae84
nando-castro <fernando.castro10.master@gmail.com>
2022-08-06 13:11:29 -0300

    fix ranking

--- src/controllers/rankingControllers.js
+++ src/controllers/rankingControllers.js
@@ -3,14 +3,16 @@ import connection from "../db/database.js";
 export async function getRanking(req, res) {
   try {
     const { rows: users } =
-     await connection.query(`SELECT u."id", u."name", COUNT(l."link") AS "linksCount", SUM(l."views") AS "visitCount" 
-    FROM "links" l
-    JOIN "users" u ON l."userId" = u."id"
-    GROUP BY u."id"
+      await connection.query(`SELECT u."id", u."name", COUNT(l."link") AS "linksCount", COALESCE(SUM(l."views"),0) AS "visitCount"
+    FROM "users" u
+    LEFT JOIN "links" l
+    ON u.id = l."userId"
+    GROUP BY u.id
     ORDER BY "visitCount" DESC
     LIMIT 10`);
+
     //const [ranking] = users;
-    res.status(200).send(users)
+    res.status(200).send(users);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
